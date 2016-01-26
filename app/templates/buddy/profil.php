c481bed03c90acbd6dd19e8b476405b7b20f6fed
francoisleuliet <francoisleuliet@gmail.com>
2016-01-26 10:49:40 +0100

    ajout profil

--- app/templates/buddy/profil.php
+++ app/templates/buddy/profil.php
@@ -3,7 +3,27 @@
 
 <?php $this->start('main_content') ?>
 
-<h1>TEST TITRE</h1>
-<div>Test</div>
+<?php
+
+try {
+$bdd = new PDO('mysql:host=localhost;dbname=buddy;charset=utf8', 'root', ''); 
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
+
+<br>
+<div>Nom : </div>
+<div>Prénom : </div>
+<div>Date de naissance : </div>
+<div>Email : </div>
+<div>Adresse : </div>
+<div>Code Postal : </div>
+<div>Ville : </div>
+<div>Téléphone : </div>
+<div>Mini Bio : </div>
+<div>Photo de Profil : </div>
 
 <?php $this->stop('main_content') ?>
